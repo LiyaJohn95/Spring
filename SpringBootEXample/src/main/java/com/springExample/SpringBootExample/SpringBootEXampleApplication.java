4b21383ee3180e731c02861048344d59a4855934
LiyaJohn95 <liyajohn2@gmail.com>
2017-09-14 07:22:55 +0000

    Add files via upload

---
+++ SpringBootEXample/src/main/java/com/springExample/SpringBootExample/SpringBootEXampleApplication.java
@@ -0,0 +1,12 @@
+package com.springExample.SpringBootExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEXampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEXampleApplication.class, args);
+	}
+}
---
+++ SpringBootEXample/src/main/java/com/springExample/SpringBootExample/controller/Login.java
@@ -0,0 +1,25 @@
+package com.springExample.SpringBootExample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Login {
+	@RequestMapping(value={"/login"}, method = RequestMethod.GET)
+	public ModelAndView login(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value={"/registration"}, method = RequestMethod.GET)
+	public ModelAndView registration(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("registration");
+		return modelAndView;
+	}
+	
+
+}
